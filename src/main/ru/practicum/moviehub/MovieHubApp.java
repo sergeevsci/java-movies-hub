7068523c48aa6049578004f67c940adceea32adf
Maxim Sergeev <sergeevsci@yandex.ru>
2026-05-17 17:56:20 +0300

    надоел мне этот CheckStyle 2

--- src/main/ru/practicum/moviehub/MovieHubApp.java
+++ src/main/ru/practicum/moviehub/MovieHubApp.java
@@ -3,10 +3,14 @@ package ru.practicum.moviehub;
 import ru.practicum.moviehub.http.MoviesServer;
 
 public class MovieHubApp {
+
     private static final int PORT = 8080;
+
     public static void main(String[] args) {
         final MoviesServer server = new MoviesServer(PORT);
         Runtime.getRuntime().addShutdownHook(new Thread(server::stop));
         server.start();
+
     }
+
 }
